91026885c95e1d8c8d173429243238a33af15f64
Sviatoslav Vasev <sviatoslav.vasev@tuni.fi>
2022-05-10 00:14:15 +0300

    finished Ex 3

--- react-challenges/src/App.js
+++ react-challenges/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Ex1 from './exercises/ex1';
 import Ex2 from './exercises/ex2';
+import Ex3 from './exercises/ex3';
 import React, {useState} from 'react';
 
 function App() {
@@ -12,18 +13,19 @@ function App() {
 
   return (
     <div className="App">
-      <button value={1} onClick={processClick}>Exercise 1</button>
-      <button value={2} onClick={processClick}>Exercise 2</button>
-      <button value={3} onClick={processClick}>Exercise 3</button>
-      <button value={4} onClick={processClick}>Exercise 4</button>
-      <button value={5} onClick={processClick}>Exercise 5</button>
-      <button value={6} onClick={processClick}>Exercise 6</button>
-      <button value={7} onClick={processClick}>Exercise 7</button>
-      <button value={8} onClick={processClick}>Exercise 8</button>
+      <button value={1} onClick={processClick}>Exercise 1</button>|
+      <button value={2} onClick={processClick}>Exercise 2</button>|
+      <button value={3} onClick={processClick}>Exercise 3</button>|
+      <button value={4} onClick={processClick}>Exercise 4</button>|
+      <button value={5} onClick={processClick}>Exercise 5</button>|
+      <button value={6} onClick={processClick}>Exercise 6</button>|
+      <button value={7} onClick={processClick}>Exercise 7</button>|
+      <button value={8} onClick={processClick}>Exercise 8</button>|
       <button value={9} onClick={processClick}>Exercise 9</button>
       <br/>
       {exerciseNum == 1 ? <Ex1/> : ""}
       {exerciseNum == 2 ? <Ex2/> : ""}
+      {exerciseNum == 3 ? <Ex3/> : ""}
     </div>
   );
 }
---
+++ react-challenges/src/exercises/ex3-Button.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './exercises.css';
+
+function MyButton(props) {
+    const processClick = (event) => {
+        if (typeof props.onClick === "function") {
+            props.onClick(event);
+        }
+    }
+    
+    return (
+        <button onClick={processClick} className='ex2-button'>{props.children}</button>
+    );
+}
+
+export default MyButton;
